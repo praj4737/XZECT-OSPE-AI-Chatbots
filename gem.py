f6867d4cd512e7111084ba7c9aba7cc43e6af18d
pranav raj <22bcad46@kristujayanti.com>
2024-08-06 18:52:12 +0530

    XZ/OSPE/2024/1029

--- gem.py
+++ gem.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
 import os
+import os
 
 genai.configure(api_key="AIzaSyDZ2hAL6ydGGpHsT2UfvORKMVi-LSYE-eI")
 # api_key="AIzaSyDZ2hAL6ydGGpHsT2UfvORKMVi-LSYE-eI"
@@ -7,3 +8,4 @@ genai.configure(api_key="AIzaSyDZ2hAL6ydGGpHsT2UfvORKMVi-LSYE-eI")
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 response = model.generate_content(["who is gandhi"])
 print(response.text)
+print()
